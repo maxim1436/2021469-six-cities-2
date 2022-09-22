2ee181315640a3be9160e638c5fd12ecab2d42e3
maxim1436 <pivanov_m@mail.ru>
2022-09-23 03:17:32 +1000

    приложение для терминала (2 часть)

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,45 +1,67 @@
-import { readFileSync } from 'fs';
-import { Offer } from '../../types/offer.type.js';
+import EventEmitter from 'events';
+// import { Offer } from '../../types/offer.type.js';
+import { createReadStream } from 'fs';
 import { FileReaderInterface } from './file-reader.interface.js';
 
-export default class TSVFileReader implements FileReaderInterface {
-  private rawData = '';
-
-  constructor(public filename: string) { }
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf8' });
+export default class TSVFileReader extends EventEmitter implements FileReaderInterface {
+  constructor(public filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      return [];
+  public async read():Promise<void> {
+    const stream = createReadStream(this.filename, {
+      highWaterMark: 16384, // 16KB
+      encoding: 'utf-8',
+    });
+
+    let lineRead = '';
+    let endLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of stream) {
+      lineRead += chunk.toString();
+
+      while ((endLinePosition = lineRead.indexOf('\n')) >= 0) {
+        const completeRow = lineRead.slice(0, endLinePosition + 1);
+        lineRead = lineRead.slice(++endLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
 
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim() !== '')
-      .map((line) => line.split('\t'))
-      .map(([bedrooms, city, description, goods, images, isFavorite, isPremium, latitude, longitude, maxAdults, previewImage, price, rating, title, type, createdDate, avatarUrl, email, password, isPro, name]) => ({
-        bedrooms: Number.parseInt(bedrooms, 10),
-        city,
-        description,
-        goods: goods.split(';')
-          .map((good) => ({good})),
-        images: images.split(';')
-          .map((img) => ({img})),
-        isFavorite: Boolean(Number.parseInt(isFavorite, 10)),
-        isPremium: Boolean(Number.parseInt(isPremium, 10)),
-        latitude: Number.parseFloat(latitude),
-        longitude: Number.parseFloat(longitude),
-        maxAdults: Number.parseInt(maxAdults, 10),
-        previewImage,
-        price: Number.parseInt(price, 10),
-        rating: Number.parseFloat(rating),
-        title,
-        type,
-        createdDate: new Date(createdDate),
-        host: {avatarUrl, email, password, isPro: Boolean(Number.parseInt(isPro, 10)), name},
-      }));
+    this.emit('end', importedRowCount);
+
+    // public toArray(): Offer[] {
+    //   if (!this.rawData) {
+    //     return [];
+    //   }
+
+    //   return this.rawData
+    //     .split('\n')
+    //     .filter((row) => row.trim() !== '')
+    //     .map((line) => line.split('\t'))
+    //     .map(([bedrooms, city, description, goods, images, isFavorite, isPremium, latitude, longitude, maxAdults, previewImage, price, rating, title, type, createdDate, avatarUrl, email, password, isPro, name]) => ({
+    //       bedrooms: Number.parseInt(bedrooms, 10),
+    //       city,
+    //       description,
+    //       goods: goods.split(';')
+    //         .map((good) => ({good})),
+    //       images: images.split(';')
+    //         .map((img) => ({img})),
+    //       isFavorite: Boolean(Number.parseInt(isFavorite, 10)),
+    //       isPremium: Boolean(Number.parseInt(isPremium, 10)),
+    //       latitude: Number.parseFloat(latitude),
+    //       longitude: Number.parseFloat(longitude),
+    //       maxAdults: Number.parseInt(maxAdults, 10),
+    //       previewImage,
+    //       price: Number.parseInt(price, 10),
+    //       rating: Number.parseFloat(rating),
+    //       title,
+    //       type,
+    //       createdDate: new Date(createdDate),
+    //       host: {avatarUrl, email, password, isPro: Boolean(Number.parseInt(isPro, 10)), name},
+    //     }));
+    // }
   }
 }
---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,11 @@
+export type MockData = {
+  cities: string[];
+  descriptions: string[];
+  goods: string[];
+  images: string[];
+  titles: string[];
+  types: string[];
+  avatarUrls: string[];
+  emails: string[];
+  names: string[];
+};
