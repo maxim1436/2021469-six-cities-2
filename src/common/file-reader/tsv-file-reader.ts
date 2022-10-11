133f29da06dc82ed7bc7762a42582275b9fa217e
maxim1436 <pivanov_m@mail.ru>
2022-10-11 22:28:48 +1000

    архитектура

--- src/common/file-reader/tsv-file-reader.ts
+++ src/common/file-reader/tsv-file-reader.ts
@@ -1,5 +1,4 @@
 import EventEmitter from 'events';
-// import { Offer } from '../../types/offer.type.js';
 import { createReadStream } from 'fs';
 import { FileReaderInterface } from './file-reader.interface.js';
 
@@ -10,7 +9,7 @@ export default class TSVFileReader extends EventEmitter implements FileReaderInt
 
   public async read():Promise<void> {
     const stream = createReadStream(this.filename, {
-      highWaterMark: 16384, // 16KB
+      highWaterMark: 16384,
       encoding: 'utf-8',
     });
 
@@ -31,37 +30,5 @@ export default class TSVFileReader extends EventEmitter implements FileReaderInt
     }
 
     this.emit('end', importedRowCount);
-
-    // public toArray(): Offer[] {
-    //   if (!this.rawData) {
-    //     return [];
-    //   }
-
-    //   return this.rawData
-    //     .split('\n')
-    //     .filter((row) => row.trim() !== '')
-    //     .map((line) => line.split('\t'))
-    //     .map(([bedrooms, city, description, goods, images, isFavorite, isPremium, latitude, longitude, maxAdults, previewImage, price, rating, title, type, createdDate, avatarUrl, email, password, isPro, name]) => ({
-    //       bedrooms: Number.parseInt(bedrooms, 10),
-    //       city,
-    //       description,
-    //       goods: goods.split(';')
-    //         .map((good) => ({good})),
-    //       images: images.split(';')
-    //         .map((img) => ({img})),
-    //       isFavorite: Boolean(Number.parseInt(isFavorite, 10)),
-    //       isPremium: Boolean(Number.parseInt(isPremium, 10)),
-    //       latitude: Number.parseFloat(latitude),
-    //       longitude: Number.parseFloat(longitude),
-    //       maxAdults: Number.parseInt(maxAdults, 10),
-    //       previewImage,
-    //       price: Number.parseInt(price, 10),
-    //       rating: Number.parseFloat(rating),
-    //       title,
-    //       type,
-    //       createdDate: new Date(createdDate),
-    //       host: {avatarUrl, email, password, isPro: Boolean(Number.parseInt(isPro, 10)), name},
-    //     }));
-    // }
   }
 }
